33fd9fb64332b3c4a452d37d96b5df969bd63aba
Kheng <kheng.h.chen@gmail.com>
2014-09-20 13:43:18 +0800

    Assignment 2
    Updated Codes

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## --------------------------------------------------
+## -------------------------------------------------
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
